e502207b7bc0179f8fee6c571759f1c926382c59
acharjeeany <anycse2006@gmail.com>
2019-09-07 11:24:11 +0600

    Calculator skeleton added

--- src/KeysComponent.js
+++ src/KeysComponent.js
@@ -6,24 +6,36 @@ class KeysComponent extends Component{
 		return(
 			<div className="buttons">
 				<h2>Calculator</h2>
+				<div><input type="text" placeholder="Output" /></div>
+				<div>
+					<button>*</button>
+					<button>/</button>
+					<button>+</button>
+					<button>-</button>
+					<button>Clear</button>
+					<button>=</button>
+				</div>
 				<div>
 					<button>1</button>
 					<button>2</button>
 					<button>3</button>
-					<button>4</button>
+					<button>Backspace</button>
 				</div>
 				<div>
+					<button>4</button>
 					<button>5</button>
 					<button>6</button>
 					<button>7</button>
 					<button>8</button>
-				</div>
-				<div>
 					<button>9</button>
 					<button>0</button>
-					<button>+</button>
-					<button>-</button>
+
 				</div>
+				<div>
+					
+					
+				</div>
+				
 			</div>
 			)
 	}
